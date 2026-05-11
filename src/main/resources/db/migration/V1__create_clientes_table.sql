ec22cb01bf58fce78d2ce7439b6cbcb5f3bf4976
Redskatex24 <ju.garridog@duocuc.cl>
2026-05-10 23:57:01 -0400

    edicion de tabla

---
+++ src/main/resources/db/migration/V1__create_clientes_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE clientes (
+    'id' int NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    numero INT DEFAULT NULL,
+    PRIMARY KEY ('id')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
