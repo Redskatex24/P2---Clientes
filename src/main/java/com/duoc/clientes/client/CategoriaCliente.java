abba33db488e3254a6bec7d4e3afd7b64d5be8fa
Redskatex <ju.garridog@duocuc.cl>
2026-06-12 22:28:32 -0400

    agregación de implementación docker

--- src/main/java/com/duoc/clientes/client/CategoriaCliente.java
+++ src/main/java/com/duoc/clientes/client/CategoriaCliente.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "categoria-cliente", url = "http://localhost:8081/api/v1")
+@FeignClient(name = "categoria-cliente", url = "http://localhost:8090/api/v1")
 public interface CategoriaCliente {
 
     @GetMapping("/categorias")
